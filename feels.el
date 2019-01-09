b9e0d7103dc98b5c449a0768355747bc9cb5f336
Ian <ian.thomas@ufl.edu>
2019-01-09 07:40:32 -0500

    Init

---
+++ feels.el
@@ -0,0 +1,6 @@
+;; TODO Feels
+;; Interactive:
+;; Get screen proportions with windows,
+;; Map to keyboard space and evaluate key pressed,
+;; Switch to respective window.
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Window-Sizes.html
